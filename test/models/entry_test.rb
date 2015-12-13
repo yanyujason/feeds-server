6a1238d567dded1cf48e1275305641c02e73289d
Shenjian YUAN <sjyuan@thoughtworks.com>
2015-12-13 22:22:12 +0800

    [#]remove the db drop from test helper

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,4 +1,4 @@
-
+require 'test_helper'
 
 class EntryTest < ActiveSupport::TestCase
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,11 +3,12 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  teardown :clean_mongodb
-
-  def clean_mongodb
-    Rake::Task['db:mongoid:purge'].invoke
-  end
+  # teardown :clean_db
+  #
+  # def clean_db
+  #   Rake::Task['db:mongoid:purge'].invoke
+  #   Rake::Task['db:mongoid:drop'].invoke
+  # end
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   # fixtures :all
